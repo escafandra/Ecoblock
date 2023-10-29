c2b56ff29e9fa4ecefdc1efbaccba69b16a1b33b
joselomanuelo <joselomanuelo@gmail.com>
2023-10-29 12:10:46 -0500

    refactor: use normal id

--- database/migrations/2023_10_27_170223_create_products_table.php
+++ database/migrations/2023_10_27_170223_create_products_table.php
@@ -5,14 +5,11 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('name', 50);
+            $table->id();
+            $table->string('name');
             $table->text('description')->nullable(true);
             $table->integer('price')->nullable(true);
             $table->json('advantages')->nullable(true);
@@ -21,9 +18,6 @@ return new class () extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
