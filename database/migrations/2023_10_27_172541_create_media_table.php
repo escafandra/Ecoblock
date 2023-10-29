7355662c2aa0f922fc4ce85728bdcd7e06965cab
joselomanuelo <joselomanuelo@gmail.com>
2023-10-29 12:00:22 -0500

    fix: add down to migration

--- database/migrations/2023_10_27_172541_create_media_table.php
+++ database/migrations/2023_10_27_172541_create_media_table.php
@@ -9,7 +9,6 @@ return new class () extends Migration {
     {
         Schema::create('media', function (Blueprint $table) {
             $table->id();
-
             $table->morphs('model');
             $table->uuid('uuid')->nullable()->unique();
             $table->string('collection_name');
@@ -27,5 +26,10 @@ return new class () extends Migration {
 
             $table->nullableTimestamps();
         });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
     }
 };
