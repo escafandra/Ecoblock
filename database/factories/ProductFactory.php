9a9e7887ca24c7f4a4b90d46a3ab59e35ea1da7b
joselomanuelo <joselomanuelo@gmail.com>
2023-10-29 12:10:13 -0500

    feat: product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,6 +9,11 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomNumber(5),
+            'advantages' => json_encode(["key" => "value"]),
+            'datasheet' => json_encode(["key" => "value"])
         ];
     }
 }
