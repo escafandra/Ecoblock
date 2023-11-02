bb9356e31d6b52df590171519e976604cacfd422
joselomanuelo <joselomanuelo@gmail.com>
2023-11-02 06:47:31 -0500

    feat: product view

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,37 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ trans('navigation.catalog') }}
-        </h2>
-    </x-slot>
-    <main class="my-8">
-        <div class="container mx-auto px-6">
-            <form action="{{ route('product.index') }}" method="GET" class="grid grid-cols-7 gap-3">
-                <x-input id="search" type="text" name="search" class="col-span-6"/>
-                <x-button class="row-span-2">
-                    {{ __(trans('buttons.search')) }}
-                </x-button>
-            </form>
-            <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-
-                @foreach ($products as $product)
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                        <div class="flex items-end justify-end h-56 w-full bg-cover">
-                            <a href="{{ route('product.show', $product) }}">
-                                <img src="{{ $product->getFirstMediaUrl('images', 'preview')}}">
-                            </a>
-                        </div>
-                        <div class="px-5 py-3" >
-                            <h3 class="text-gray-700 uppercase">{{ $product->name }}</h3>
-                            <span class="text-gray-500 mt-2">{{ $product->price }}</span>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-            <div class="m-4">
-                {{ $products->links() }}
-            </div>
+    <div class="text-center">
+        <h2 class="section-heading text-uppercase">Services</h2>
+        <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+    </div>
+    <div class="row text-center">
+        @foreach ($products as $product)
+        <div class="col-md-4">
+            <img class="rounded-circle img-fluid" src="{{ $product->getFirstMediaUrl('images', 'preview')}}" alt="{{ $product->name }}" />
+            <h4 class="my-3">{{ $product->name }}</h4>
+            <p class="text-muted">{{ $product->description }}</p>
         </div>
-    </main>
+        @endforeach
+    </div>
+
 @endsection
