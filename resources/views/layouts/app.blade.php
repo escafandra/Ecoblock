7a2ecacbf084bdf8ab9bf7df619eba3bd5fcd85d
elisaulperezperez <nuevo.correo.para.git@gmail.com>
2023-11-01 22:00:09 -0500

    feat: navigation links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,59 +37,12 @@
                     <i class="fas fa-bars ms-1"></i>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    @guest
-                    @else
-                    <ul class="navbar-nav me-auto">
-                            <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
-                                {{ __(trans('navigation.about')) }}
-                            </x-nav-link>
-                            <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
-                                {{ __(trans('navigation.products')) }}
-                            </x-nav-link>
-                            <x-nav-link :href="route('project.index')" :active="request()->routeIs('project.index')">
-                                {{ __(trans('navigation.projects')) }}
-                            </x-nav-link>
-                            <x-nav-link :href="route('contact')" :active="request()->routeIs('contact')">
-                                {{ __(trans('navigation.contact')) }}
-                            </x-nav-link>
-                    </ul>
-                    @endguest
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">{{ (trans('navigation.about')) }}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('product.index') }}">{{ (trans('navigation.products')) }}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('project.index') }}">{{ (trans('navigation.projects')) }}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">{{ (trans('navigation.contact')) }}</a></li>
                     </ul>
                 </div>
             </div>
@@ -98,6 +51,5 @@
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
 </body>
 </html>
