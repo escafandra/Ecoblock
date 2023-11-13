e3caca717e8179f8388add608c9313cf73e00fcb
joselomanuelo <joselomanuelo@gmail.com>
2023-11-13 16:06:10 -0500

    fix: location map dimension

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -39,7 +39,7 @@
                 </div>
             </form>
             <div class="ratio ratio-21x9">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4488.777804928391!2d-74.01082320743795!3d4.767527992347781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f8f33c22a4633%3A0xd32bd7a6391835da!2sEcological%20Block%20Systems!5e0!3m2!1ses-419!2sco!4v1699907101542!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4488.777804928391!2d-74.01082320743795!3d4.767527992347781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f8f33c22a4633%3A0xd32bd7a6391835da!2sEcological%20Block%20Systems!5e0!3m2!1ses-419!2sco!4v1699907101542!5m2!1ses-419!2sco" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </section>
