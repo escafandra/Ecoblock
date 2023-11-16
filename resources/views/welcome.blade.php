006e0c198af98f57e38623e7e98b2f3155856b14
joselomanuelo <joselomanuelo@gmail.com>
2023-11-16 18:08:45 -0500

    feat: welcome videos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,6 +32,118 @@
                     </button>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/6jcG1xGi1rw?si=ZVQl_zhzzpx7NFRh" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/gcs4QFNl1oo?si=z7Fl1Km3svgZ3V06" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/AavvAgTCo5Q?si=qZr57F4RzJ1d3nih" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/7Ut_OveFF1w?si=4KI3FVoPl052rQxb" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/wFDnVa0YaEk?si=Gqd1stH2dfi8ST65" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/EkT9u1LLrBw?si=4VgM3qaxUWB5gBGc" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/9B8LZfvYes8?si=fxmupjbiByLPbQ9N" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/J0iHOiNXHEU?si=GqSj5S64t97Knx1h" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/5n2Ed7BTrcs?si=oz07of2Z5ZGwHF4c" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <div class="ratio ratio-16x9">
+                            <iframe class="rounded" src="https://www.youtube.com/embed/bLbHrHvwDZ0?si=OgXo2Otp-4joR5pE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-title">Instalación</div>
+                            <div class="text-muted card-text">Así se hace</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
